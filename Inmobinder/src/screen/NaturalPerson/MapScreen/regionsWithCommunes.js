c59f6d32362030afe6fd0a19c829247836f04649
Dantrottel <daniel.aguayo2001@alumnos.ubiobio.cl>
2024-07-01 14:37:42 -0400

    ultimos cambios

---
+++ Inmobinder/src/screen/NaturalPerson/MapScreen/regionsWithCommunes.js
@@ -0,0 +1,70 @@
+// regionsWithCommunes.js
+
+const regionesYComunas = [
+    {
+      region: 'Región de Arica y Parinacota',
+      comunas: ['Arica', 'Putre']
+    },
+    {
+      region: 'Región de Tarapacá',
+      comunas: ['Iquique', 'Alto Hospicio']
+    },
+    {
+      region: 'Región de Antofagasta',
+      comunas: ['Antofagasta', 'Calama', 'Tocopilla']
+    },
+    {
+      region: 'Región de Atacama',
+      comunas: ['Copiapó', 'Vallenar']
+    },
+    {
+      region: 'Región de Coquimbo',
+      comunas: ['La Serena', 'Coquimbo', 'Ovalle']
+    },
+    {
+      region: 'Región de Valparaíso',
+      comunas: ['Valparaíso', 'Viña del Mar', 'Quilpué', 'Villa Alemana']
+    },
+    {
+      region: 'Región Metropolitana de Santiago',
+      comunas: ['Santiago', 'Maipú', 'Puente Alto', 'Providencia']
+    },
+    {
+      region: 'Región del Libertador General Bernardo O\'Higgins',
+      comunas: ['Rancagua', 'San Fernando']
+    },
+    {
+      region: 'Región del Maule',
+      comunas: ['Talca', 'Curicó']
+    },
+    {
+      region: 'Región de Ñuble',
+      comunas: ['Chillán', 'Bulnes']
+    },
+    {
+      region: 'Región del Biobío',
+      comunas: ['Concepción', 'Talcahuano', 'Los Ángeles']
+    },
+    {
+      region: 'Región de La Araucanía',
+      comunas: ['Temuco', 'Angol']
+    },
+    {
+      region: 'Región de Los Ríos',
+      comunas: ['Valdivia', 'La Unión']
+    },
+    {
+      region: 'Región de Los Lagos',
+      comunas: ['Puerto Montt', 'Osorno', 'Castro']
+    },
+    {
+      region: 'Región de Aysén del General Carlos Ibáñez del Campo',
+      comunas: ['Coyhaique', 'Puerto Aysén']
+    },
+    {
+      region: 'Región de Magallanes y de la Antártica Chilena',
+      comunas: ['Punta Arenas', 'Puerto Natales']
+    }
+  ];
+  
+  export default regionesYComunas;
